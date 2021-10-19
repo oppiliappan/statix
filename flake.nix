a1886854d9fa5f02fe755352b44f6aabf2711514
Akshay <nerdy@peppe.rs>
2021-10-19 20:39:11 +0530

    fix flake builds

--- flake.nix
+++ flake.nix
@@ -1,80 +1,102 @@
 {
   inputs = {
-    utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nmattia/naersk";
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
     mozillapkgs = {
       url = "github:mozilla/nixpkgs-mozilla";
       flake = false;
     };
+
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
+
+    import-cargo.url = github:edolstra/import-cargo;
+
   };
 
   outputs =
     { self
     , nixpkgs
-    , utils
-    , naersk
     , mozillapkgs
+    , import-cargo
     , ...
     }:
-    utils.lib.eachDefaultSystem (system:
     let
-      pkgs = nixpkgs.legacyPackages."${system}";
-
-      # Get a specific rust version
-      mozilla = pkgs.callPackage (mozillapkgs + "/package-set.nix") { };
+      inherit (import-cargo.builders) importCargo;
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        });
+      mozilla = p: p.callPackage (mozillapkgs + "/package-set.nix") { };
       chanspec = {
-        date = "2021-03-31";
+        date = "2021-09-30";
         channel = "nightly";
-        sha256 = "oK5ebje09MRn988saJMT3Zze/tRE7u9zTeFPV1CEeLc="; # set zeros after modifying channel or date
+        sha256 = "Elqn7GDBDE/QT1XTDyj0EvivbC//uwjWX8d+J3Pi0dY="; # set zeros after modifying channel or date
       };
-
-      rustChannel = mozilla.rustChannelOf chanspec;
-      rust = rustChannel.rust;
-      rust-src = rustChannel.rust-src;
-
-      naersk-lib = naersk.lib."${system}".override {
-        cargo = rust;
-        rustc = rust;
-      };
-
-      nativeBuildInputs = with pkgs; [ ];
-
     in
-    rec {
-      packages.statix = naersk-lib.buildPackage {
-        pname = "statix";
-        version = "0.1.0";
-        root = ./.;
-        inherit nativeBuildInputs;
-      };
+    {
 
-      defaultPackage = packages.statix;
-      apps.statix = utils.lib.mkApp {
-        drv = packages.statix;
-      };
+      overlay = final: prev:
+        let
+          rustChannel = (mozilla final.pkgs).rustChannelOf chanspec;
+          inherit (rustChannel) rust rustc rust-src;
+        in
+        {
 
-      apps.check = {
-        type = "app";
-        program = "${pkgs.cargo-watch}/bin/cargo-watch";
-      };
+          statix = with final; pkgs.stdenv.mkDerivation {
+            pname = "statix";
+            version = "v0.1.0";
+            src = ./.;
+            nativeBuildInputs = [
+              (importCargo { lockFile = ./Cargo.lock; inherit pkgs; }).cargoHome
+              rust
+              cargo
+            ];
+            buildPhase = ''
+              cargo build -p statix --release --offline
+            '';
+            installPhase = ''
+              install -Dm775 ./target/release/statix $out/bin/statix
+            '';
+          };
 
-      defaultApp = apps.statix;
-      devShell = pkgs.mkShell {
-        nativeBuildInputs = nativeBuildInputs ++ [
-          rust
-          rust-src
-          pkgs.rust-analyzer
-          pkgs.rustfmt
-          pkgs.cargo
-          pkgs.cargo-watch
-        ];
-        RUST_SRC_PATH = "${rust-src}/lib/rustlib/src/rust/library";
-        RUST_LOG = "info";
-        RUST_BACKTRACE = 1;
-      };
-    });
+        };
+
+      packages = forAllSystems (system: {
+        inherit (nixpkgsFor."${system}") statix;
+      });
+
+      defaultPackage =
+        forAllSystems (system: self.packages."${system}".statix);
+
+      devShell = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          rustChannel = (mozilla pkgs).rustChannelOf chanspec;
+        in
+        with pkgs;
+        mkShell rec {
+          buildInputs =
+            (with pkgs; [
+              rust-analyzer
+              rustfmt
+              cargo
+              cargo-watch
+            ]) ++ (with rustChannel; [
+              rust
+              rust-src
+            ]);
+          RUST_SRC_PATH = "${rustChannel.rust-src}/lib/rustlib/src/rust/library";
+          RUST_LOG = "info";
+          RUST_BACKTRACE = 1;
+        });
+
+
+    };
 }
