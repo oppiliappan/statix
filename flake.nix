43964c400850237927544df5d5a13a33150717af
Tobias Happ <tobias.happ@gmx.de>
2021-12-06 18:21:55 +0100

    add aarch64-linux as supported system in flake.nix

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     let
       inherit (gitignore.lib) gitignoreSource;
 
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system:
         import nixpkgs {
